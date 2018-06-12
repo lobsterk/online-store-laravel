c0237f8067b4a131ae4e97461e082fd230607209
Kopylov Danil <lobsterk@yandex.ru>
2018-06-12 23:23:16 +0300

    add product: migration, model

---
+++ src/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'title', 'html_h1', 'html_title', 'meta_keywords',
+        'meta_description', 'description', 'model',
+        'price', 'count', 'status', 'created_by'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function manufacturer()
+    {
+        return $this->belongsTo('App\Models\Manufacturer');
+    }
+}
---
+++ src/database/migrations/2018_06_12_221045_create_products_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->integer('manufacturer_id')->unsigned();
+            $table->string('title');
+            $table->string('html_h1');
+            $table->string('html_title');
+            $table->string('meta_keywords');
+            $table->string('meta_description');
+            $table->string('description');
+            $table->string('model')->unique();
+            $table->float('price');
+            $table->integer('count');
+            $table->boolean('status');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('manufacturer_id')
+                ->references('id')->on('manufacturers')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_category_id_foreign');
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_manufacturer_foreign');
+        });
+        Schema::dropIfExists('products');
+    }
+}
